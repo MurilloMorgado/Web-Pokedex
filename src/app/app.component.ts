e6df8ad0c7d67a57a5b41d9f21d3b7dc22f1c8f1
Murillo Morgado <mumorgadok@hotmail.com>
2024-03-13 22:02:35 -0300

    preparando seletores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PainelAnimeComponent } from './views/painel-anime/painel-anime.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, PainelAnimeComponent],
+  template: `
+  <router-outlet></router-outlet>
+  <app-painel-anime/>
+  
+  `,
+  
+
 })
 export class AppComponent {
   title = 'pokedex';
