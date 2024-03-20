0cf3d54da20f1f0177f52df18a909f7f61d4dd83
Murillo Morgado <mumorgadok@hotmail.com>
2024-03-20 19:24:46 -0300

    responsividade

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { PainelAnimeComponent } from './views/painel-anime/painel-anime.componen
 import { LoginComponent } from './views/login/login.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/anime', pathMatch: 'full' },
   { path: 'anime', component: PainelAnimeComponent },
   { path: 'login', component: LoginComponent },
 ];
