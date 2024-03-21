e8337b3d24bf3173bbeed67208452128bfbf3465
Murillo Morgado <mumorgadok@hotmail.com>
2024-03-21 20:41:09 -0300

    ajustado sessoes, iniciando ajuste footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,7 +9,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
 
 }
