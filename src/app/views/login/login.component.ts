3612e733c00ea6e2d993837faf7e5514aab2b03f
Murillo Morgado <mumorgadok@hotmail.com>
2024-04-03 20:42:00 -0300

    Ajustes no painel de login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule,FormsModule,ButtonModule,InputSwitchModule,PasswordModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  
+  value: string | undefined;
+  password: string | undefined;
+  checked: boolean = false;
+
+  ngOnInit(): void {
+    
+  }
 
 }
